15d530ad48847b9b2d80d950b1618a5b0edf1c6b
Morozov Ivan <96567271+morozoffivan@users.noreply.github.com>
2023-11-08 12:39:08 +0300

    Add files via upload

--- testSelenium1/smook_test.py
+++ testSelenium1/smook_test.py
@@ -31,9 +31,10 @@ get_url = driver.current_url
 assert main_url == get_url
 #Делаем потверждающий скриншот c текущей датой
 now_date = datetime.datetime.utcnow().strftime('%d.%m.%Y_%H.%M')
-name_screen_main = f'Вход {now_date}.png'
+name_screen_main = f'Шаг_1_Вход {now_date}.png'
 time.sleep(1)
-driver.save_screenshot('C:\\Users\\ivanm\\PycharmProjects\\pythonProject\\testSelenium1\\' + name_screen_main)
+driver.save_screenshot('C:\\Users\\ivanm\\PycharmProjects\\Selenium\\smoke_test_shop-main\\smoke_test_shop-main\\'
+                       'testSelenium1\\screen\\' + name_screen_main)
 print('Вход подтвержден')
 #Выбираем 1 продукт для добавление в корзину
 product_1 = driver.find_element(By.XPATH, '//*[@id="item_4_title_link"]/div')
@@ -61,6 +62,104 @@ print(f'Товар {value_product_2} добавлен в корзину')
 cart = driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a')
 cart.click()
 print('Переход в корзину прошел успешно')
-name_screen_cart = f'Корзина {now_date}.png'
-driver.save_screenshot('C:\\Users\\ivanm\\PycharmProjects\\pythonProject\\testSelenium1\\' + name_screen_cart)
-#Сверяем названия в каталоге с названием в корзине
+name_screen_cart = f'Шаг_2_Корзина {now_date}.png'
+driver.save_screenshot('C:\\Users\\ivanm\\PycharmProjects\\Selenium\\smoke_test_shop-main\\smoke_test_shop-main\\'
+                       'testSelenium1\\screen\\' + name_screen_cart)
+#Сверяем названия в каталоге с названием в корзине 1 продукта
+product_1_cart = driver.find_element(By.XPATH, '//*[@id="item_4_title_link"]/div')
+value_product_1_cart = product_1_cart.text
+print(value_product_1_cart)
+assert value_product_1_cart == value_product_1
+print('Продукт 1 соответствует выбранному')
+#Сверяем названия в каталоге с названием в корзине 2 продукта
+product_2_cart = driver.find_element(By.XPATH, '//*[@id="item_2_title_link"]/div')
+value_product_2_cart = product_2_cart.text
+print(value_product_2_cart)
+assert value_product_2_cart == value_product_2
+print('Продукт 2 соответствует выбранному')
+#Сверяем цену товара 1 в корзине
+product_price_1_cart = driver.find_element(By.XPATH,
+                                           '//*[@id="cart_contents_container"]/div/div[1]/div[3]/'
+                                           'div[2]/div[2]/div')
+value_price_product_1_cart = product_price_1_cart.text
+print(value_price_product_1_cart)
+assert value_price_product_1_cart == value_price_product_1
+print('Цена товара 1 в корзине соответствует')
+#Сверяем цену товара 2 в корзине
+product_price_2_cart = driver.find_element(By.XPATH,
+                                           '//*[@id="cart_contents_container"]/div/div[1]/div[4]/'
+                                           'div[2]/div[2]/div')
+value_price_product_2_cart = product_price_2_cart.text
+print(value_price_product_2_cart)
+assert value_price_product_2_cart == value_price_product_2
+print('Цена товара 2 в корзине соответствует')
+#Продолжаем оформление заказа. Нажимаем на кнопку оформления заказа
+checkout_btn = driver.find_element(By.XPATH, '//button[@id="checkout"]')
+checkout_btn.click()
+#Вводим форму для формления заказа (Имя, Фамилия, Индекс почты)
+first_name_var = "Иван"
+last_name_var = 'Морозов'
+postal_code_var = 12345
+first_name = driver.find_element(By.XPATH, '//input[@id="first-name"]')
+first_name.send_keys(first_name_var)
+last_name = driver.find_element(By.XPATH, '//input[@id="last-name"]')
+last_name.send_keys(last_name_var)
+postal_code = driver.find_element(By.XPATH, '//input[@id="postal-code"]')
+postal_code.send_keys(postal_code_var)
+#Делаем скриншот заполненой формы
+name_screen_cart = f'Шаг_3_Форма_заполнения_заказа {now_date}.png'
+driver.save_screenshot('C:\\Users\\ivanm\\PycharmProjects\\Selenium\\smoke_test_shop-main\\smoke_test_shop-main\\'
+                       'testSelenium1\\screen\\' + name_screen_cart)
+#Кликаем на кнопку продолжающий оформление заказа
+continue_btn = driver.find_element(By.XPATH, '//input[@id="continue"]')
+continue_btn.click()
+#Делаем скриншот финишного этапа формления заказа
+name_screen_cart = f'Шаг_4_Финиш {now_date}.png'
+driver.save_screenshot('C:\\Users\\ivanm\\PycharmProjects\\Selenium\\smoke_test_shop-main\\smoke_test_shop-main\\'
+                       'testSelenium1\\screen\\' + name_screen_cart)
+#Сверяем названия в финишной форме с каталогом и с названием в корзине 1 продукта
+product_1_finish = driver.find_element(By.XPATH, '//*[@id="item_4_title_link"]/div')
+value_product_1_finish = product_1_finish.text
+print(value_product_1_finish)
+assert value_product_1_finish == value_product_1 == value_product_1_finish
+print('Названия в финишной форме с каталогом и с названием в корзине 1 продукта соответствует')
+#Сверяем цену в финишной форме с каталогом и с названием в корзине 2 продукта
+product_2_finish = driver.find_element(By.XPATH, '//*[@id="item_2_title_link"]/div')
+value_product_2_finish = product_2_finish.text
+print(value_product_2_finish)
+assert value_product_2_cart == value_product_2 == value_product_2_finish
+print('Цена в финишной форме с каталогом и с названием в корзине 2 продукта соответствует')
+#Достаем стоимость вычесленную в финальной форме
+price_total = driver.find_element(By.XPATH, '//*[@id="checkout_summary_container"]/div/div[2]/div[6]')
+price_total_value_finish = price_total.text
+print(price_total_value_finish)
+#Проведем сложение цен 1 товара и 2 товара. Уберем из строки с ценой знак $
+value_price_product_1_finish = float(value_price_product_1.strip('$'))
+value_price_product_2_finish = float(value_price_product_2.strip('$'))
+total_value_price = f'Item total: ${value_price_product_1_finish + value_price_product_2_finish}'
+print(total_value_price)
+#Сверяем стоимость товаров
+assert price_total_value_finish == total_value_price
+print('Полная стоимость заказа верна')
+#Оформляем заказ. Нажимаем на кнопку финального оформления заказа
+btn_finish = driver.find_element(By.XPATH, '//button[@id="finish"]')
+btn_finish.click()
+
+#Сверяем адрес финиша оформления заказа с текущим адресом
+finish_url = 'https://www.saucedemo.com/checkout-complete.html'
+get_finish_url = driver.current_url
+name_screen_cart = f'Шаг_5_Заказ_оформлен {now_date}.png'
+driver.save_screenshot('C:\\Users\\ivanm\\PycharmProjects\\Selenium\\smoke_test_shop-main\\smoke_test_shop-main\\'
+                       'testSelenium1\\screen\\' + name_screen_cart)
+assert finish_url == get_finish_url
+print('Заказ создан')
+#Нажимаем на кнопку возврата на главную страницу
+btn_home = driver.find_element(By.XPATH, '//button[@id="back-to-products"]')
+btn_home.click()
+name_screen_cart = f'Шаг_6_Возврат_на_главную {now_date}.png'
+driver.save_screenshot('C:\\Users\\ivanm\\PycharmProjects\\Selenium\\smoke_test_shop-main\\smoke_test_shop-main\\'
+                       'testSelenium1\\screen\\' + name_screen_cart)
+back_home_url = main_url
+get_back_home_url = driver.current_url
+assert back_home_url == get_back_home_url
+print('Возврат на главную страницу осуществлен успешно')
